334a009bbf7c7a453330787d9ae48c22b26ea9ae
RA <relian.skype@yandex.ru>
2025-11-25 15:19:17 +0300

    Загрузка кода

---
+++ lettestfor.js
@@ -0,0 +1,11 @@
+//TODO посмотреть позже, после освоения основ react
+var div,
+	container = document.getElementById("container");
+
+for(var i=0; i<5; i++) {
+	div = document.createElement("div");
+	div.onclick = function() {
+		alert("This is box #" + i);
+	};
+	container.appendChild(div);
+}
